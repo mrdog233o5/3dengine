dff060d935c8e5495ed2f9f794ef4b7f0544e6ae
mrdog233o5 <mr.dog233o5@gmail.com>
2024-06-05 20:42:32 +0800

    changed code structure & made canva auto fullscreen

--- src/main.js
+++ src/main.js
@@ -22,53 +22,67 @@ void main() {
 }
 `;
 
-initCanvas();
 
-function initCanvas() {
-    var canvas = document.getElementById("canvas");
-    var gl = canvas.getContext("webgl2");
+// init
+var canvas = document.getElementById("canvas");
+var gl = canvas.getContext("webgl2");
+if (!gl) {
+    // Fall back to WebGL 1.0 if WebGL 2.0 is not available
+    gl = canvas.getContext("webgl");
     if (!gl) {
-        // Fall back to WebGL 1.0 if WebGL 2.0 is not available
-        gl = canvas.getContext("webgl");
-        if (!gl) {
-            console.error("WebGL is not supported in this browser.");
-            return;
-        }
-        console.log("Using WebGL 1.0");
-    } else {
-        console.log("Using WebGL 2.0");
+        console.error("WebGL is not supported in this browser.");
     }
+    console.log("Using WebGL 1.0");
+} else {
+    console.log("Using WebGL 2.0");
+}
 
-    gl.clearColor(0, 0, 0, 1);
+gl.clearColor(0, 0, 0, 1);
+
+var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+gl.shaderSource(vertexShader, vertexShaderText);
+gl.shaderSource(fragmentShader, fragmentShaderText);
+
+gl.compileShader(vertexShader);
+if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error("Couldn't compile vertex shader:", gl.getShaderInfoLog(vertexShader));
+}
+gl.compileShader(fragmentShader);
+if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error("Couldn't compile fragment shader:", gl.getShaderInfoLog(fragmentShader));
+}
+
+var program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+gl.useProgram(program);
+
+// 
+// stuff
+// 
+
+gl.enable(gl.DEPTH_TEST);
+gl.depthFunc(gl.LESS);
+gl.depthMask(true);
+
+// 
+// main render loop
+// 
+function frame() {
+    // set canvas size
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // clear screen
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-    gl.shaderSource(vertexShader, vertexShaderText);
-    gl.shaderSource(fragmentShader, fragmentShaderText);
-
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.error("Couldn't compile vertex shader:", gl.getShaderInfoLog(vertexShader));
-    }
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.error("Couldn't compile fragment shader:", gl.getShaderInfoLog(fragmentShader));
-    }
-
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-	
-    gl.useProgram(program);
-
-    // 
-    // buffers
-    // 
     var lenPerRow = 6;
-    var triangleVerticies = new Float32Array([
+    triangleVerticies = new Float32Array([
         0.3, 0.5, -0.5,        1, 1, 0,
         -0.5, 0, 0,         1, 1, 0,
         0.3, -0.5, 0.5,       1, 1, 0,
@@ -80,10 +94,10 @@ function initCanvas() {
     var triangleVertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
     gl.bufferData(gl.ARRAY_BUFFER, triangleVerticies, gl.STATIC_DRAW);
-	
-	const positionAttribLocation = gl.getAttribLocation(program, 'vertPos');
-	const colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
-
+    
+    const positionAttribLocation = gl.getAttribLocation(program, 'vertPos');
+    const colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+    
     gl.vertexAttribPointer(
         positionAttribLocation,
         3,
@@ -100,16 +114,13 @@ function initCanvas() {
         lenPerRow * Float32Array.BYTES_PER_ELEMENT,
         3 * Float32Array.BYTES_PER_ELEMENT
     );
-
+    
     gl.enableVertexAttribArray(positionAttribLocation);
     gl.enableVertexAttribArray(colorAttribLocation);
 
-    // 
-    // main render loop
-    // 
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LESS);
-    gl.depthMask(true);
     gl.drawArrays(gl.TRIANGLES, 0, triangleVerticies.length / lenPerRow);
-    
+
+    // loop
+    requestAnimationFrame(frame);
 }
+frame();
