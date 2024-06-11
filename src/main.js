3b29bac920f554e1917d46611442961c9f7963ae
mrdog233o5 <mr.dog233o5@gmail.com>
2024-06-11 11:10:01 +0800

    made triangles filled

--- src/main.js
+++ src/main.js
@@ -2,52 +2,51 @@
 
 // load stuff
 var map = [
-    1, 1, 1,
-    1, 1, 3,
-    1, -1, 3,
+    1, 1, 1, 1, 0, 1,
+    1, 1, 3, 1, 0, 1,
+    1, -1, 3, 1, 0, 1,
 
-    1, 1, 1,
-    1, -1, 1,
-    1, -1, 3,
+    1, 1, 1, 1, 0, 1,
+    1, -1, 1, 1, 0, 1,
+    1, -1, 3, 1, 0, 1,
 
-    -1, 1, 1,
-    -1, 1, 3,
-    -1, -1, 3,
+    -1, 1, 1, 1, 0, 1,
+    -1, 1, 3, 1, 0, 1,
+    -1, -1, 3, 1, 0, 1,
 
-    -1, 1, 1,
-    -1, -1, 1,
-    -1, -1, 3,
+    -1, 1, 1, 1, 0, 1,
+    -1, -1, 1, 1, 0, 1,
+    -1, -1, 3, 1, 0, 1,
     
-    1, 1, 1,
-    1, 1, 3,
-    -1, 1, 3,
+    1, 1, 1, 0, 1, 1,
+    1, 1, 3, 0, 1, 1,
+    -1, 1, 3, 0, 1, 1,
 
-    1, 1, 1,
-    -1, 1, 1,
-    -1, 1, 3,
+    1, 1, 1, 0, 1, 1,
+    -1, 1, 1, 0, 1, 1,
+    -1, 1, 3, 0, 1, 1,
 
-    1, -1, 1,
-    1, -1, 3,
-    -1, -1, 3,
+    1, -1, 1, 0, 1, 1,
+    1, -1, 3, 0, 1, 1,
+    -1, -1, 3, 0, 1, 1,
 
-    1, -1, 1,
-    -1, -1, 1,
-    -1, -1, 3,
+    1, -1, 1, 0, 1, 1,
+    -1, -1, 1, 0, 1, 1,
+    -1, -1, 3, 0, 1, 1,
 
-    1, 1, 3,
-    1, -1, 3,
-    -1, -1, 3,
+    1, 1, 3, 1, 1, 0,
+    1, -1, 3, 1, 1, 0,
+    -1, -1, 3, 1, 1, 0,
 
-    1, 1, 3,
-    -1, 1, 3,
-    -1, -1, 3,
+    1, 1, 3, 1, 1, 0,
+    -1, 1, 3, 1, 1, 0,
+    -1, -1, 3, 1, 1, 0,
 ];
 
 const canvas1 = new DoggyGraphicsEngine(document.getElementById("canvas"));
 
 canvas1.frame = () => {
-    var camAngle = 0;
-    var lineLength = 3;
+    var lineLength = 6;
 	
     // loop through all triangles
     for (let i = 0; i < map.length; i += lineLength*3) {
@@ -59,15 +58,12 @@ canvas1.frame = () => {
             var rotated = canvas1.calcRotatedCoord2D(canvas1.loops*2, [0, 0.5]);
             c[0] += rotated[0];
             c[1] += rotated[1];
-            vertices.push(canvas1.projecting3D(canvas1.fov, [canvas.width, canvas.height], canvas1.renderingRange, c).concat([1, 1, 0]));
+            vertices.push(canvas1.projecting3D(canvas1.fov, [canvas.width, canvas.height], canvas1.renderingRange, c).concat(map.slice(j+3, j+lineLength)));
         }
-        canvas1.lineVertices = canvas1.lineVertices
+        canvas1.triangleVertices = canvas1.triangleVertices
             .concat(vertices[0])
             .concat(vertices[1])
-            .concat(vertices[1])
-            .concat(vertices[2])
-            .concat(vertices[2])
-            .concat(vertices[0]);
+            .concat(vertices[2]);
     }
 }
 canvas1.renderingFrame();
