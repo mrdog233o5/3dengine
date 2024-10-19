8c213da70223266165c3fa3413c0bbc00116500e
mrdog233o5 <mr.dog233o5@gmail.com>
2024-10-19 13:35:16 +0800

    remove(removed function initColorBuffer() as theres texture and its no longer used, /src/initBuffers.js)

--- src/initBuffers.js
+++ src/initBuffers.js
@@ -49,33 +49,6 @@ function initPositionBuffer(gl) {
 
 	return positionBuffer;
 }
-function initColorBuffer(gl) {
-	const faceColors = [
-		[1.0, 1.0, 1.0, 1.0], // Front face: white
-		[1.0, 0.0, 0.0, 1.0], // Back face: red
-		[0.0, 1.0, 0.0, 1.0], // Top face: green
-		[0.0, 0.0, 1.0, 1.0], // Bottom face: blue
-		[1.0, 1.0, 0.0, 1.0], // Right face: yellow
-		[1.0, 0.0, 1.0, 1.0], // Left face: purple
-	];
-
-	var colors = [];
-
-	for (var j = 0; j < faceColors.length; ++j) {
-		const c = faceColors[j];
-		colors = colors.concat(c, c, c, c);
-	}
-
-	const colorBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.bufferData(
-		gl.ARRAY_BUFFER,
-		new Float32Array(colors),
-		gl.STATIC_DRAW,
-	);
-
-	return colorBuffer;
-}
 
 function initIndexBuffer(gl) {
 	const indexBuffer = gl.createBuffer();
