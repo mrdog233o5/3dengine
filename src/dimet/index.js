871bee7fce479534aa4e9003fcf2e68e64ac78c0
mrdog233o5 <mr.dog233o5@gmail.com>
2024-10-21 14:55:57 +0800

    core(modulized the code, put all the initialization in a new file, /src/dimet/initCanvas.js)

--- src/dimet/index.js
+++ src/dimet/index.js
@@ -1,5 +1,6 @@
 import { initBuffers } from "./initBuffers.js";
 import { renderObject } from "./renderObject.js";
+import { initWebGL, initShader, initProgramProps } from "./initCanvas.js";
 
 let cubeRotation = 0.0;
 
@@ -10,91 +11,12 @@ class Dimet {
 	}
 
 	init() {
-		this.initWebGL();
-		this.initShaderProgram();
-		this.initProgramProps();
-	}
-
-	initWebGL() {
-		this.gl = canvas.getContext("webgl2");
-
-		if (this.gl === null) {
-			console.error(
-				"Unable to initialize Webgl. Your browser or machine may not support it.",
-			);
-			return;
-		}
-	}
-
-	initShaderProgram() {
-		const vsSource = `#version 300 es
-		in vec4 aVertexPosition;
-		in vec2 aTextureCoord;
-	
-		uniform mat4 uModelViewMatrix;
-		uniform mat4 uProjectionMatrix;
-	
-		out highp vec2 vTextureCoord;
-	
-		void main(void) {
-			  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-			  vTextureCoord = aTextureCoord;
-		}
-		  `;
-
-		const fsSource = `#version 300 es
-		precision highp float;
-		in vec2 vTextureCoord;
-	
-		uniform sampler2D uSampler;
-		out vec4 fragColor;
-	
-		void main(void) {
-			fragColor = texture(uSampler, vTextureCoord);
-			// this.fragColor = vec4(1,1,1,1);
-		}
-		`;
-
-		this.shaderProgram = initShaderProgram(
+		this.gl = initWebGL(this.canvas);
+		this.shaderProgram = initShader(this.gl);
+		this.programProps = initProgramProps(
 			this.gl,
-			vsSource,
-			fsSource,
+			this.shaderProgram,
 		);
-	}
-
-	initProgramProps() {
-		this.programProps = {
-			program: this.shaderProgram,
-			attribLocations: {
-				vertexPosition: this.gl.getAttribLocation(
-					this
-						.shaderProgram,
-					"aVertexPosition",
-				),
-				textureCoord: this.gl.getAttribLocation(
-					this
-						.shaderProgram,
-					"aTextureCoord",
-				),
-			},
-			uniformLocations: {
-				projectionMatrix: this.gl.getUniformLocation(
-					this
-						.shaderProgram,
-					"uProjectionMatrix",
-				),
-				modelViewMatrix: this.gl.getUniformLocation(
-					this
-						.shaderProgram,
-					"uModelViewMatrix",
-				),
-				uSampler: this.gl.getUniformLocation(
-					this
-						.shaderProgram,
-					"uSampler",
-				),
-			},
-		};
 	}
 
 	drawObject(
@@ -103,7 +25,7 @@ class Dimet {
 		indices,
 		translationMatrix,
 		rotationMatrix,
-		textureURL
+		textureURL,
 	) {
 		this.objects.push([
 			vertexPos,
@@ -111,8 +33,86 @@ class Dimet {
 			indices,
 			translationMatrix,
 			rotationMatrix,
-			loadTexture(this.gl, textureURL)
+			this.loadTexture(
+				this.gl,
+				textureURL,
+			),
 		]);
+	}
+
+	loadTexture(gl, url) {
+		const texture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+
+		const level = 0;
+		const internalFormat = gl.RGBA;
+		const width = 1;
+		const height = 1;
+		const border = 0;
+		const srcFormat = gl.RGBA;
+		const srcType = gl.UNSIGNED_BYTE;
+		const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+		gl.texImage2D(
+			gl.TEXTURE_2D,
+			level,
+			internalFormat,
+			width,
+			height,
+			border,
+			srcFormat,
+			srcType,
+			pixel,
+		);
+
+		const image = new Image();
+		image.onload = () => {
+			gl.bindTexture(
+				gl.TEXTURE_2D,
+				texture,
+			);
+			gl.texImage2D(
+				gl.TEXTURE_2D,
+				level,
+				internalFormat,
+				srcFormat,
+				srcType,
+				image,
+			);
+
+			if (
+				(image.width &
+					(image.width -
+						1)) ===
+					0 &&
+				(image.height &
+					(image.height -
+						1)) ===
+					0
+			) {
+				gl.generateMipmap(
+					gl.TEXTURE_2D,
+				);
+			} else {
+				gl.texParameteri(
+					gl.TEXTURE_2D,
+					gl.TEXTURE_WRAP_S,
+					gl.CLAMP_TO_EDGE,
+				);
+				gl.texParameteri(
+					gl.TEXTURE_2D,
+					gl.TEXTURE_WRAP_T,
+					gl.CLAMP_TO_EDGE,
+				);
+				gl.texParameteri(
+					gl.TEXTURE_2D,
+					gl.TEXTURE_MIN_FILTER,
+					gl.LINEAR,
+				);
+			}
+		};
+		image.src = url;
+
+		return texture;
 	}
 
 	render() {
@@ -150,7 +150,8 @@ class Dimet {
 				buffers,
 				object[5],
 				cubeRotation,
-				object[2].length,
+				object[2]
+					.length,
 				object[3],
 				object[4],
 			);
@@ -158,117 +159,6 @@ class Dimet {
 
 		cubeRotation += 0.01;
 	}
-}
-
-function initShaderProgram(gl, vsSource, fsSource) {
-	const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-	const fragmentShader = loadShader(
-		gl,
-		gl.FRAGMENT_SHADER,
-		fsSource,
-	);
-
-	const shaderProgram = gl.createProgram();
-	gl.attachShader(shaderProgram, vertexShader);
-	gl.attachShader(shaderProgram, fragmentShader);
-	gl.linkProgram(shaderProgram);
-
-	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-		console.error(
-			`Unable to initialize the shader program: ${gl.getProgramInfoLog(
-				shaderProgram,
-			)}`,
-		);
-		return null;
-	}
-
-	return shaderProgram;
-}
-
-function loadShader(gl, type, source) {
-	const shader = gl.createShader(type);
-
-	gl.shaderSource(shader, source);
-
-	gl.compileShader(shader);
-
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		console.error(
-			`An error occurred compiling the shaders: ${gl.getShaderInfoLog(
-				shader,
-			)}`,
-		);
-		gl.deleteShader(shader);
-		return null;
-	}
-
-	return shader;
-}
-
-function loadTexture(gl, url) {
-	const texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-
-	const level = 0;
-	const internalFormat = gl.RGBA;
-	const width = 1;
-	const height = 1;
-	const border = 0;
-	const srcFormat = gl.RGBA;
-	const srcType = gl.UNSIGNED_BYTE;
-	const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
-	gl.texImage2D(
-		gl.TEXTURE_2D,
-		level,
-		internalFormat,
-		width,
-		height,
-		border,
-		srcFormat,
-		srcType,
-		pixel,
-	);
-
-	const image = new Image();
-	image.onload = () => {
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.texImage2D(
-			gl.TEXTURE_2D,
-			level,
-			internalFormat,
-			srcFormat,
-			srcType,
-			image,
-		);
-
-		if (
-			isPowerOf2(image.width) &&
-			isPowerOf2(image.height)
-		) {
-			gl.generateMipmap(
-				gl.TEXTURE_2D,
-			);
-		} else {
-			gl.texParameteri(
-				gl.TEXTURE_2D,
-				gl.TEXTURE_WRAP_S,
-				gl.CLAMP_TO_EDGE,
-			);
-			gl.texParameteri(
-				gl.TEXTURE_2D,
-				gl.TEXTURE_WRAP_T,
-				gl.CLAMP_TO_EDGE,
-			);
-			gl.texParameteri(
-				gl.TEXTURE_2D,
-				gl.TEXTURE_MIN_FILTER,
-				gl.LINEAR,
-			);
-		}
-	};
-	image.src = url;
-
-	return texture;
 }
 
 function isPowerOf2(value) {
